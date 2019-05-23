4727b97b9f1c75a7871b489e6009006ba1595dde
ZHANG Mengxin <untraveledz@gmail.com>
2019-05-23 08:10:29 +0200

    naive test

--- UnitTest/unittest1.cpp
+++ UnitTest/unittest1.cpp
@@ -1,8 +1,11 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "CppUnitTest.h"
 #include "string"
+#include "vector"
+#include "set"
 #include "../ATMO/Capteur.h"
-#in
+#include "../ATMO/TypeMesure.h"
+#include "../ATMO/TraitementDonnees.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,17 +14,31 @@ namespace UnitTest
 	TEST_CLASS(RecupereationDesDonnees)
 	{
 	public:
-		TEST_METHOD(RecupereationDesDonnees) 
+		TEST_METHOD(TypeMesure) 
 		{
-			TraitementDeDonnees
-			Assert::AreEqual(toString, "Sensor1 Latitude:-8.15758888291083 Longitude:-34.7692487876719");
+			TraitementDonnees t;
+			TypeMesure t1 ("O3", "µg/m3", "concentration d'ozone");
+			TypeMesure t2("SO2", "µg/m3" ,"concentration de dioxyde de soufre");
+			TypeMesure t3("NO2", "µg/m3","concentration de dioxyde d'azote");
+			TypeMesure t4("PM10", "µg/m3", "concentration de particules fines");
+			std::set<TypeMesure> s1{ t1,t2,t3,t4 };
+			std::vector<TypeMesure> v = t.ParcoursTypesMesure();
+			std::set<TypeMesure> s2(v.begin(), v.end());
+			Assert::IsTrue(s1==s2);
 		}
-		TEST_METHOD(ConstructeurParDefaut)
+		TEST_METHOD(Capteur)
 		{
-			Capteur c;
-			std::string toString = c.toString();
-			Assert::AreEqual(toString, "Sensor1 Latitude:-8.15758888291083 Longitude:-34.7692487876719");
+			TraitementDonnees t;
+			Capteur c1("O3", "µg/m3", "concentration d'ozone");
+			Capteur c2("SO2", "µg/m3", "concentration de dioxyde de soufre");
+			Capteur c3("NO2", "µg/m3", "concentration de dioxyde d'azote");
+			Capteur c4("PM10", "µg/m3", "concentration de particules fines");
+			C
+			std::set<TypeMesure> s1{ t1,t2,t3,t4 };
+			std::vector<TypeMesure> v = t.ParcoursTypesMesure();
+			std::set<TypeMesure> s2(v.begin(), v.end());
+			Assert::IsTrue(s1 == s2);
 		}
-		TEST8
+		
 	};
 }
