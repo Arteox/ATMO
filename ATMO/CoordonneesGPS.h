a038e40ea731088b4ffa5cb622558331d253080d
Louis <ung.louis@hotmail.fr>
2019-05-31 21:41:02 +0200

    création des classes Zone, CoordonneesGPS et Menu

---
+++ ATMO/CoordonneesGPS.h
@@ -0,0 +1,76 @@
+/*************************************************************************
+CoordonneesGPS  -  description
+-------------------
+début                : 07/05/2019
+copyright            : (C) 2019 par Mengxing ZHANG, Louis UNG, Fabien GELUS et Baptiste PAULETTO
+*************************************************************************/
+
+//---------- Interface de la classe <CoordonneesGPS> (fichier CoordonneesGPS.h) ------
+#ifndef _COORDONNEESGPS_H
+#define _COORDONNEESGPS_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+//------------------------------------------------------------------------ 
+// Rôle de la classe <CoordonneesGPS>
+//
+//
+//------------------------------------------------------------------------ 
+class CoordonneesGPS
+{
+	//----------------------------------------------------------------- PUBLIC
+public:
+	//----------------------------------------------------- Méthodes publiques
+
+
+	//------------------------------------------------- Surcharge d'opérateurs
+	// Mode d'emploi :
+	// RAS
+	// Contrat : Aucun
+	//
+
+	//-------------------------------------------- Constructeurs - destructeur
+	CoordonneesGPS(const CoordonneesGPS & unCoordonneesGPS);
+	// Mode d'emploi (constructeur de copie) :
+	// RAS
+	// Contrat : Aucun
+	//
+
+	CoordonneesGPS();
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+	virtual ~CoordonneesGPS();
+	// Mode d'emploi : RAS, contenu vide
+	//
+	// Contrat : Aucun
+	//
+	//------------------------------------------------------------------ PRIVE
+
+protected:
+	//----------------------------------------------------- Méthodes protégées
+
+private:
+	//------------------------------------------------------- Méthodes privées
+
+protected:
+	//----------------------------------------------------- Attributs protégés
+
+private:
+	//------------------------------------------------------- Attributs privés
+
+	//---------------------------------------------------------- Classes amies
+
+	//-------------------------------------------------------- Classes privées
+
+	//----------------------------------------------------------- Types privés
+};
+
+//----------------------------------------- Types dépendants de <CoordonneesGPS>
+
+#endif //_COORDONNEESGPS_H
