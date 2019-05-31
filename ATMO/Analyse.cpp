454bf4fe6452feebc00a011f20bce26c5c84e0a8
Fabien GELUS <45175277+Fabien-GELUS@users.noreply.github.com>
2019-05-31 10:51:45 +0200

    caracteristiquesZone et qualiteAir à tester

--- ATMO/Analyse.cpp
+++ ATMO/Analyse.cpp
@@ -35,26 +35,53 @@ conteneurMoyMesures Analyse::caracteristiquesZone(bool d, double lat, double lon
 	conteneurMoyMesures moyMesures;
 	
 	collectionCapteurs capteurs;
-	//todo : typedef TraitementDonnesInstance
+
 	capteurs = TraitementD.ParcoursCapteurs(lat, longi, rayon);
-	//todo : typedef collectionMesures
+
 	collectionMesures mesures;
 	mesures = TraitementD.ParcoursMesures(capteurs,horodateDeb, horodateFin);
 
+	//collectionTypesMesure types = TraitementD.ParcoursTypesMesure();
+
+
+
 	collectionMesures ::iterator it;  // declare an iterator to a vector of strings
 
-	for (it = mesures.begin(); it != mesures.end(); it++) 
-	{	
-		
-		it->getTypeMesure().getAttributeId();
-		
-			
-			
+	double moyO3 = 0;
+	double moySO2 = 0;
+	double moyNO2 = 0;
+	double moyPM10 = 0;
+
+	for (it = mesures.begin(); it != mesures.end(); it++)
+	{
+
+		string type = it->getTypeMesure().getAttributeId();
+		double valeur = it->getValeur();
+		if (type == "O3") {
+			moyO3 += valeur;
+		}
+		else if (type == "SO2") {
+			moySO2 += valeur;
+		}
+		else if (type == "NO2") {
+			moyNO2 += valeur;
+		}
+		else if (type == "PM10") {
+			moyPM10 += valeur;
+		}
 	}
-
+	
+	paireMoyMesures paire;
+	paire = paireMoyMesures("O3", moyO3);
+	moyMesures.insert(paire); 
+	paire = paireMoyMesures("SO2", moyO3);
+	moyMesures.insert(paire);
+	paire = paireMoyMesures("NO2", moyO3);
+	moyMesures.insert(paire);
+	paire = paireMoyMesures("PM10", moyO3);
+	moyMesures.insert(paire);
 	
 	
-
 	return moyMesures;
 
 }
@@ -80,7 +107,34 @@ conteneurMoyMesures Analyse::caracteristiquesPoint(double long, double lat, Date
 
 int Analyse::qualiteAir(conteneurMoyMesures MoyMesures)
 {
-	return int();
+	int indice = 0;
+
+	string types[4] = { "O3", "SO2", "NO2", "PM10" };
+	double atmo[4][9] = { (30,55,80,105,130,150,180,210,240), (40,80,120,160,200,250,300,400,500), (30,55,85,110,135,165,200,275,400), (7,14,21,28,35,42,50,65,80) };
+
+	for (int i = 0; i < 4; i++) {
+		double valeur = MoyMesures.find(types[i])->second;
+
+		if (valeur < 0)
+		{
+			return -1; // erreur 
+		}
+
+		for (int j = 0; j < 9; j++) {
+			if (valeur < atmo[i][j] && indice < j + 1)
+			{
+				indice = j + 1;
+			}
+		}
+
+		if (valeur > atmo[i][8] && indice < 10)
+		{
+			indice = 10;
+		}
+		
+	}
+	
+	return indice;
 }
 
 //------------------------------------------------- Surcharge d'opérateurs
