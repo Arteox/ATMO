1c9d40b1cbe8d70a8d8fcbf2a7696f8936045c77
Baptiste Pauletto <baptiste.pauletto@etu.univ-amu.fr>
2019-06-01 12:12:08 +0200

    Ajout des fonctionnalités de la classe Menu
    Classe Menu complète (.h) & (.cpp)

--- ATMO/Menu.cpp
+++ ATMO/Menu.cpp
@@ -28,8 +28,38 @@ using namespace std;
 
 //----------------------------------------------------- Méthodes publiques
 
+void Menu::AffichageSortieStandard(string & ligne) const {
+	sortieStandard << ligne << endl;
+}
 
-//------------------------------------------------- Surcharge d'opérateurs
+void Menu::AffichageSortieErreur(string & ligne) const {
+	sortieErreur << ligne << endl;
+}
+
+void Menu::InitialiserMenu() const {
+	sortieStandard << "Bienvenue sur ATMO ! " << endl
+	 << " Les commandes dont vous disposez sont :" << endl
+	 << " qm : Pour connaitre la qualite moyenne de l'air " << endl
+	 << " sim : Afin de trouver des zones ayant des caractéristiques similaires " << endl
+	 << " dysfonc : Afin d'essayer de repérer les capteurs qui sont en dysfonctionnement " << endl
+	 << " carac : Pour connaître les caractéristiques spécifiques d'une point précis " << endl
+	 << " help : pour obtenir plus d'informations sur les parametres des commandes " << endl;
+}
+
+void Menu::AfficherAide() const {
+	sortieStandard << "Commandes détaillées :" << endl
+	 << " Qualite moyenne : qm [-d] 'lat' 'long' 'rayon' 'horodateDeb' 'horodateFin'" << endl
+	 << " Capteurs similaires : sim 'horodateDeb' 'horodateFin'" << endl
+	 << " Capteurs dysfonctionnants : dysfonc 'horodateDeb' 'horodateFin'" << endl
+	 << " Valeurs caractéristiques d'un point : carac 'long' 'lat' 'horodateDeb' 'horodateFin' " <<endl;
+
+}
+
+string & Menu::RecupererEntreeUtilisateur() {
+	string entreeUtilisateur;
+	cin >> entreeUtilisateur;
+	return entreeUtilisateur;
+}
 
 
 //-------------------------------------------- Constructeurs - destructeur
--- ATMO/Menu.h
+++ ATMO/Menu.h
@@ -12,23 +12,56 @@ copyright            : (C) 2019 par Mengxing ZHANG, Louis UNG, Fabien GELUS et B
 //--------------------------------------------------- Interfaces utilisées
 
 //------------------------------------------------------------- Constantes 
-
+#define sortieStandard cout; // constantes pouvant être modifiée à l'avenir.
+#define sortieErreur cerr; // si l'on souhaite travailler dans un fichier par ex.
 //------------------------------------------------------------------ Types 
 //------------------------------------------------------------------------ 
 // Rôle de la classe <Menu>
-//
-//
+// Le rôle de la classe Menu est de donner l'aspect visuel à toutes les
+// interactions avec l'utilisateur, on retrouve donc notamment, l'affichage
+// et la saisie de commande qui sera alors par la suite parsée. 
+// Elle constitue une partie essentielle de la partie Vue du projet.
 //------------------------------------------------------------------------ 
 class Menu
 {
 	//----------------------------------------------------------------- PUBLIC
 public:
 	//----------------------------------------------------- Méthodes publiques
+	void AffichageSortieStandard(string & ligne) const;
+	// Mode d'emploi : Cette méthode permet au main de gérer la partie vue en
+	// affichant à l'écran l'information passé en paramètre à la méthode.
+	// string & ligne : c'est cette chaîne de caractère qui sera affichée sur la 
+	// sortie standard.
+	// Contrat : Aucun
+	//
 
+	void AffichageSortieErreur(string & ligne) const;
+	// Mode d'emploi : Cette méthode permet au main de gérer la partie vue en
+	// affichant à l'écran des messages d'erreur, en effet, la ligne passée en 
+	// paramètre sera envoyée sur la sortie d'erreur.
+	// string & ligne : c'est cette chaîne de caractère qui sera affichée sur la 
+	// sortie standard.
+	// Contrat : Aucun
+	//
 
-	//------------------------------------------------- Surcharge d'opérateurs
-	// Mode d'emploi :
-	// RAS
+	void AfficherAide() const;
+	// Mode d'emploi : Cette méthode permet d'afficher sur la sortie standard
+	// l'aide contenant les commandes ainsi que leurs paramètres et,
+	// si elles en ont, leurs options.
+	// Contrat : Aucun
+	//
+
+	void InitialiserMenu() const;
+	// Mode d'emploi : Cette méthodea pour simple objectif d'afficher sur la sortie 
+	// standard, le message d'accueil d'un utilisateur et de lui expliquer brièvement
+	// comment utiliser l'application ATMO.
+	// Contrat : Aucun
+	//
+
+	string & RecupererEntreeUtilisateur();
+	// Mode d'emploi : Cette méthode renvoie sous la forme d'une chaîne de caractères,
+	// ce qui a été tapé par l'utilisateur, afin de permettre de parser la commande, 
+	// puis d'agir en conséquence.
 	// Contrat : Aucun
 	//
 
@@ -53,13 +86,10 @@ public:
 	//------------------------------------------------------------------ PRIVE
 
 protected:
+	//----------------------------------------------------- Attributs protégés
+
 	//----------------------------------------------------- Méthodes protégées
 
-private:
-	//------------------------------------------------------- Méthodes privées
-
-protected:
-	//----------------------------------------------------- Attributs protégés
 
 private:
 	//------------------------------------------------------- Attributs privés
